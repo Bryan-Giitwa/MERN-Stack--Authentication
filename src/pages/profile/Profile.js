40d1420f8c42cb6aebe27b89e5bee5e39c382ca4
Bryan-Giitwa <giitwab4@gmail.com>
2024-06-21 19:04:09 +0300

    Adding new data

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -14,10 +14,26 @@ const initialState = {
 };
 
 const Profile = () => {
-  const [profile, setProfile] = useState(initialState);
+  const [profile, setProfile] = useState(profileImg);
+  const [formData, setFormData] = useState(initialState);
 
-  const handleImageChange = () => {};
-  const handleInputChange = () => {};
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfile(reader.result);
+        setFormData((prevState) => ({
+          ...prevState,
+          photo: reader.result,
+        }));
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <section>
@@ -25,35 +41,60 @@ const Profile = () => {
           <h2>Profile</h2>
           <div className="--flex-start profile">
             <Card cardClass={"card"}>
-              <div>
-                <div className="profile-photo">
-                  <img src={profileImg} alt="profileimg" />
-                  <h3>Role: Subscriber</h3>
+              <>
+                <div>
+                  <div className="profile-photo">
+                    <div>
+                      <img src={profile} alt="profileimg" />
+                      <h3>Role: Subscriber</h3>
+                    </div>
+                  </div>
+                  <form>
+                    <label>Change Photo</label>
+                    <input
+                      type="file"
+                      accept="image/*"
+                      name="image"
+                      onChange={handleImageChange}
+                    />
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleInputChange}
+                    />
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleInputChange}
+                    />
+                    <label>Phone</label>
+                    <input
+                      type="text"
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleInputChange}
+                    />
+                    <label>Bio</label>
+                    <textarea
+                      name="bio"
+                      cols={30}
+                      rows={10}
+                      value={formData.bio}
+                      onChange={handleInputChange}
+                    ></textarea>
+                    <button
+                      type="submit"
+                      className="--btn --btn-primary --btn-block"
+                    >
+                      update Profile
+                    </button>
+                  </form>
                 </div>
-                <form>
-                  <label>Change Photo</label>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    name="iamge"
-                    onChange={handleImageChange}
-                  />
-                  <label>Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    value={profile.name}
-                    onChange={handleInputChange}
-                  />
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    value={profile.email}
-                    onChange={handleInputChange}
-                  />
-                </form>
-              </div>
+              </>
             </Card>
           </div>
         </div>
